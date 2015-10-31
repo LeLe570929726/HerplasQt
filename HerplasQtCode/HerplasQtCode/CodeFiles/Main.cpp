b61a733bc77eab3b946b5ca1c0bbdbd7c449519c
LeLe <570929726@qq.com>
2015-10-31 10:14:52 +0800

    add new branch and replace lots of thing

--- HerplasQtCode/HerplasQtCode/CodeFiles/Main.cpp
+++ HerplasQtCode/HerplasQtCode/CodeFiles/Main.cpp
@@ -1,49 +1,37 @@
-﻿/************************************************************
-** Copyright ® 2015 LeLe570929726. All rights reserved.
-**
-** Project: HerplasQt-Code
-** Intro: Qt is a powerful and easy framework. It is also
-**              cross-platform. But it's UI editing part is not very
-**              friendly. So, this project is present! In IDE part, you can
-**              generate UI easily. Of course, it's visualized edit. Then, 
-**              IDE can make an UI file. In coding part, program can
-**              analyze the UI file and make the UI automatically in 
-**              program. It includes Event system and Factory System 
-**              and many other utililities.
-** Licence: This project is open-source. And it follow
-**                     GNU Lesser General Public License v3. You can
-**                     modify and distribute freely as long as you follow
-**                     the  license.
-************************************************************/
-/************************************************************
-** Author: LeLe570929726
-** Time: 2015/09/19 19:47
-************************************************************/
+﻿/*
+@Copyright: Copyright ® 2015 LeLe570929726. All rights reserved.
 
-/************************************************************
-** Include part
-************************************************************/
+@Project: HerplasQt - Code
+@Intro: Qt is a powerful and easy framwork. It is also cross-platform. But it's UI editing and coding is not very friendly.
+                  So, this project is present! In IDE utility, you can generate UI easily. Of course, it's visualized editing. Then, IDE
+				  can make an UI file. In coding utility, you can analyze the UI file and make the UI automatically in program. It
+				  includes Event System and Factory System and many other utilities.
+@Licence: This project is open-source. And it follow GNU Lesser General Public License v3. You can modify and
+                         distribute freely as long as you follow the license.
+*/
+/*
+@Author: LeLe570929726
+@Last Change Time: 2015/10/31 10:04
+*/
+
+/*
+@Intro: Includes
+*/
 #include <QApplication>
-#include <QFile>
-#include <QDebug>
-#include "Base\Base.h"
 
-/************************************************************
-** Function
-**
-** Name: main
-** Parameter: argc - parameter size
-**                            argv - parameter text
-** Returned Value: none
-** Intro: Program's entry.
-************************************************************/
+/*
+@Type: Function
+
+@Name: main
+@Paramter: argc - Parameter size
+                             argv - Parameter test
+@Return: none
+@Intro: Program's entry
+*/
 int main(int argc, char *argv[]) {
-	QApplication a(argc, argv);								// Create app object.
+	// Create app object
+	QApplication a(argc, argv);
 
-	QFile xmlFile(":/HerplasQtCode/Xml/TestXml.xml");
-	xmlFile.open(QFile::ReadOnly);
-	FFactory::create(xmlFile.readAll());
-	xmlFile.close();
-
-	return a.exec();								// Into message loop.
+	// Into event loop
+	return a.exec();
 }
